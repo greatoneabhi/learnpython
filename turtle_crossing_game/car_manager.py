b9e1a98fd493e2381358ed16a25b89b433eb75ed
Abhishek Singh <abhishek.dumca@gmail.com>
2024-08-13 17:02:11 +0530

    turtle crossing game

---
+++ turtle_crossing_game/car_manager.py
@@ -0,0 +1,33 @@
+import random
+from turtle import Turtle
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 5
+
+
+class CarManager:
+
+    def __init__(self):
+        self.cars: list[Turtle] = []
+        self.speed = STARTING_MOVE_DISTANCE
+
+    def add_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle()
+            new_car.shape("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            y_pos = random.randint(-250, 250)
+            new_car.goto(300, y_pos)
+            new_car.penup()
+            self.cars.append(new_car)
+
+    def move(self):
+        for car in self.cars:
+            car.backward(self.speed)
+
+    def level_up(self):
+        self.speed += MOVE_INCREMENT
---
+++ turtle_crossing_game/scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.score = 0
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.goto(-200, 250)
+        self.write(f"Score: {self.score}", False, "center", FONT)
+
+    def add_point(self):
+        self.score += 1
+        self.update_score()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", False, "center", FONT)
